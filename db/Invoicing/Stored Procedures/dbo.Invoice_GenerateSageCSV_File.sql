0cd1846a3735d56758d04990fc9be28ba05b79d5
StuartBV <stuart@bevalued.co.uk>
2019-04-08 15:21:36 +0100

    Sage export refafctor

--- db/Invoicing/Stored Procedures/dbo.Invoice_GenerateSageCSV_File.sql
+++ db/Invoicing/Stored Procedures/dbo.Invoice_GenerateSageCSV_File.sql
@@ -10,25 +10,28 @@ set nocount on
 set ansi_warnings off
 set xact_abort on
 set dateformat dmy
-
+return
 declare @date varchar(20), @errors int=0, @I0rows int, @bcp varchar(500), @path varchar(100)	
 
-set @date= replace(replace( (convert(char(8),getdate(),3) + '-' + convert(char(8),getdate(),114)),':',''),'/','')
+set @date= Replace(Replace( (Convert(char(8),GetDate(),3) + '-' + Convert(char(8),GetDate(),114)),':',''),'/','')
 
-select @path=case dbo.servertype() when 'dev' then ppd3.dbo.LocalPath() + 'invoiceExports\V2\' else '\\tau\InvoiceExports\V2\' end
+select @path=case dbo.ServerType() when 'dev' then PPD3.dbo.LocalPath() + 'invoiceExports\V2\' else '\\tau\InvoiceExports\V2\' end
 
 truncate table Worktable_InvoiceExport_Sage
-		insert into Worktable_InvoiceExport_Sage (Transtype,AccountRef,SalesCode,Department,InvoiceDate,InvoiceNumber,Reference,Amount,VatCode,Vat)
-		select TransType,'"'+AccountRef+'"',SalesCode,Department,InvoiceDate,InvoiceNumber,Reference,Amount,VatCode,Vat
-		from Sage_Invoices
-		where Channel=@channel and AccountRef=isnull(@accountRef,AccountRef)
+		insert into Worktable_InvoiceExport_Sage (TransType, AccountRef ,SalesCode, Department, InvoiceDate, InvoiceNumber, Reference, Amount, VatCode, Vat)
+		select TransType, AccountRef, SalesCode, Department, InvoiceDate, InvoiceNumber, Reference, Amount, VatCode, Vat
+		from SageInvoices_Version2
+		where Channel=@channel and AccountRef=IsNull(@accountRef,AccountRef)
 
-set @I0rows=@@rowcount
+set @I0rows=@@RowCount
 
 if @I0rows>0
 begin
-	set @bcp= ppd3.dbo.BcpCommand() + 'Invoicing.dbo.Worktable_InvoiceExport_Sage out "' + @path + 'INV_' + @date +'.csv" -c -t, -S' +cast(serverproperty('servername') as varchar)+  ' -T'
+	set @bcp= PPD3.dbo.BcpCommand() + 'Invoicing.dbo.Worktable_InvoiceExport_Sage out "' + @path + 'INV_' + @date +'.csv" -c -t, -S' +Cast(ServerProperty('servername') as varchar)+  ' -T'
 	exec master.dbo.xp_cmdshell @bcp--,no_output
-	set @errors=@@error
+	set @errors=@@Error
 end
+
+--exec Invoice_GenerateSageCSV_File_OLD @Channel=@channel, @AccountRef=@AccountRef	-- Enable to run old version as well
+
 GO
