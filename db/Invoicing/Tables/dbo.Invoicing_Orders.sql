19deb12d99eae261afc20675ab3c2f642ec2ce16
StuartBV <stuart@bevalued.co.uk>
2019-04-09 11:19:38 +0100

    Excess for sage exports

--- db/Invoicing/Tables/dbo.Invoicing_Orders.sql
+++ db/Invoicing/Tables/dbo.Invoicing_Orders.sql
@@ -18,6 +18,7 @@ CREATE TABLE [dbo].[Invoicing_Orders]
 [OrderSentDate] [datetime] NULL,
 [SageSentDate] [smalldatetime] NULL,
 [BordereauSentDate] [smalldatetime] NULL,
+[ExcessCollected] [decimal] (5, 2) NOT NULL CONSTRAINT [DF_Invoicing_Orders_ExcessCollected] DEFAULT ((0)),
 [CreateDate] [datetime] NOT NULL CONSTRAINT [DF__INVOICINGOrders_CreateDate] DEFAULT (getdate()),
 [CreatedBy] [dbo].[UserID] NOT NULL,
 [AlteredDate] [datetime] NULL,
@@ -26,9 +27,9 @@ CREATE TABLE [dbo].[Invoicing_Orders]
 GO
 ALTER TABLE [dbo].[Invoicing_Orders] ADD CONSTRAINT [PK_INVOICING_Order] PRIMARY KEY CLUSTERED  ([Id]) WITH (FILLFACTOR=100) ON [PRIMARY]
 GO
-CREATE NONCLUSTERED INDEX [BordereauNo] ON [dbo].[Invoicing_Orders] ([BordereauNo]) ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [BordereauNo] ON [dbo].[Invoicing_Orders] ([BordereauNo]) WITH (FILLFACTOR=95) ON [PRIMARY]
 GO
-CREATE NONCLUSTERED INDEX [IX_DeliveryID] ON [dbo].[Invoicing_Orders] ([DeliveryId], [Id], [CreateDate]) ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [IX_DeliveryID] ON [dbo].[Invoicing_Orders] ([DeliveryId], [Id], [CreateDate]) WITH (FILLFACTOR=95) ON [PRIMARY]
 GO
 CREATE NONCLUSTERED INDEX [SageSentDate] ON [dbo].[Invoicing_Orders] ([SageSentDate], [Channel], [SourceType]) INCLUDE ([DeliveryId], [InscoID], [Reference], [SupplierId]) WITH (FILLFACTOR=95) ON [PRIMARY]
 GO
